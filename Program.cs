a957e7bda290d063ee14ee8e57383cce1b1718d2
Ana Finotelli <e107ti@cps.sp.gov.br>
2022-11-21 21:29:14 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Tabuada do número:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int s = 0;
+int produto;
+
+while(s <= 10)
+{
+produto = c * s;
+Console.WriteLine($"{c} x {s} = {produto}");
+s += 1;
+}
